d74a3106a55213dacf6af9fc1665f5a5ec2f2f7d
Tshiamo <bokabatia@gmai.com>
2022-12-14 10:30:45 -0800

    /bin/bash

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet: displays letters from 'a' - 'z'
+ */
+
+void print_alphabet(void)
+{
+
+	for (int letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+
+}
