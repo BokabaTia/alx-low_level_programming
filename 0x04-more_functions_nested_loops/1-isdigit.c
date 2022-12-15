bb9eb39669292c9f9ab1d6030d609ab429a8baa5
Tshiamo <bokabatia@gmai.com>
2022-12-15 11:50:01 -0800

    /bin/bash

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _isupper- Checks whether integer is a digit or not
- * @c: integer parameter
+ * _isupper- Checks whether character is a digit or not
+ * @c: character being checked
  * Return: Either 1 or 0 depending on the integer 'c'
  */
 
-int _isdigit(int c)
+int _isdigit(char c)
 {
-	if (c >= 48 && c <= 57)
+	if (c >= '9' && c <= '9')
 		return (1);
 	return (0);
 }
