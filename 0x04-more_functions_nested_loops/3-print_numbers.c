605510e5cc652d3873c6d25d2f468ce42f451193
Tshiamo <bokabatia@gmai.com>
2022-12-15 17:49:52 -0800

    /bin/bash

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new lin
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	for (char a = '0'; a < '10'; a++)
+		_putchar(a);
+
+	_putchar('\n');
+}
+
