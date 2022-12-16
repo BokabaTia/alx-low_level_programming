fce78d23f856931af3e550c806357acdb0e3d18f
Tshiamo <bokabatia@gmai.com>
2022-12-15 19:51:28 -0800

    /bin/bash

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints the numbers, from 0 to 14, 10 times
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	char a;
+	char b = '\0';
+	int idx = 0;
+	int isMore;
+
+	while (idx < 10)
+	{
+		a = '0';
+		b = '0';
+		isMore = 0;
+		while (a <= '>')
+		{
+
+			isMore = (a >= ':' && a <= '>') ? 1 : 0;
+
+			_putchar(isMore ? '1' : a);
+
+			if (isMore)
+			{
+				_putchar(b++);
+			}
+			else
+			{
+				_putchar('\0');
+			}
+			a++;
+		}
+		isMore = 0;
+		_putchar('\n');
+		idx++;
+	}
+}
+
