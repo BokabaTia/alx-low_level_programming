f3002bd748c8750e7cd98c480784409cdd3b7e89
Tshiamo <bokabatia@gmai.com>
2022-12-20 13:06:02 -0800

    /bin/bash

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,31 @@
 #include "main.h"
 
+/**
+ * strSize - gets the length of the string
+ * @str: pointer to char, a string of characters
+ * Return: integer
+**/
+
+int strSize(char *str)
+{
+	int count = 0;
+
+	while (*str++)
+	{
+		count++;
+	}
+
+	return (count);
+}
 
 /**
  * print_rev - prints a string, in reverse, followed by a new line
  * @str: pointer to char, a string of characters
  * Return: void
 **/
-
 void print_rev(char *str)
 {
-	int len = _strlen(str);
+	int len = strSize(str);
 
 	while (len > -1)
 	{
