1a4d8ec9470e09003733c47cad45c52aef58fd28
Tshiamo <bokabatia@gmai.com>
2022-12-20 11:56:21 -0800

    /bin/bash

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,13 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter and 
+ * reset_to_98 - takes a pointer to an int as parameter and
  * updates the value it points to to 98
  * @n: pointer to an int argument
  * Return: void
 **/
 
-void reset_to_98(int* n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
- 
-
