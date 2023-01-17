4173b8c0681f93128c118255c5d5108b8f7fd789
Tshiamo <bokabatia@gmai.com>
2023-01-17 22:13:59 +0000

    short_git

---
+++ 0x0E-structures-typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - struct with member objects name, age and owner
+* @name: 1st
+* @age: 2nd
+* @owner: 3rd
+*
+* Playing around with structs
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif DOG_H
