e1a38971ec5a0b60e96dfcc874277775c5f4271e
Tshiamo <bokabatia@gmai.com>
2023-01-18 11:21:02 -0800

    short_git

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: function that prints the name
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,5 +2,6 @@
 #define FUNCTION_POINTER_H
 
 void array_iterator(int *array, size_t size, void (*action)(int));
+void print_name(char *name, void (*f)(char *));
 
 #endif
